909a4630aaa04c55f354c73e15d3a5fc7850f496
Michal <marzec1992@gmail.com>
2019-01-21 21:39:46 +0100

    test(command-files-generate class): Add test for spinner

--- elo/app.imports.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-require('./src/styles.scss');
-
-require('./src/app/app.module');
-require('./src/app/app.routes');
-
-require('./src/app/components/root/root.component');
-require('./src/app/components/page-home/page-home.component');
--- elo/src/app/app.module.js
+++
@@ -1,3 +0,0 @@
-(() => {
-    module.exports = angular.module('Elo', ['ui.router']);
-})();
