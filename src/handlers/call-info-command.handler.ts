4cd64fe5b94bd61b9399ea0c3c275dc43bb67cdf
Michal <marzec1992@gmail.com>
2019-06-21 13:26:38 +0200

    docs(info command): Update info command content

--- src/handlers/call-info-command.handler.ts
+++ src/handlers/call-info-command.handler.ts
@@ -24,8 +24,8 @@ export async function callInfoCommandHandler() {
         `- generates new project; \n` +
         `usage: go to the directory where you want to initiate new project, type ${chalk.green('ajs new YourProjectName')} and hit enter;\n` +
         `options: \n` +
-        `   * ${chalk.green('-style="scss/less/css"')} - defines your style extension, default: css;\n` +
-        `   * ${chalk.green('-dependencies="true/false"')} - defines if AngularJS CLI should install dependencies for your new project, default: true;\n`
+        `   * ${chalk.green('--style scss/less/css')} - defines your style extension, default: css;\n` +
+        `   * ${chalk.green('--dependencies true/false')} - defines if AngularJS CLI should install dependencies for your new project, default: true;\n`
     );
 
     console.log(
@@ -36,7 +36,7 @@ export async function callInfoCommandHandler() {
         `usage: go to your project\'s root directory (or any directory you want), type ${chalk.green('ajs generate schema-type YourSchemaName')} and hit enter;\n` +
         `schema-type: you can generate component, directive, filter, constant, service, factory or provider;\n` +
         `options: \n` +
-        `   * ${chalk.green('-style="scss/less/css"')} - defines your schema\'s style extension, if you are inside your project created with AngularJS CLI you can\'t create schema with different style extension than defined while creating new project;\n` +
+        `   * ${chalk.green('--style scss/less/css')} - defines your schema\'s style extension, if you are inside your project created with AngularJS CLI you can\'t create schema with different style extension than defined while creating new project;\n` +
         `   * ${chalk.green('--skip-import')} - if this flag has been added to your command your new schema won\'t be imported to the main file (and not compiled by webpack), this flag works only if schema was generated inside project created with AngularJS CLI;\n`
     );
 }
